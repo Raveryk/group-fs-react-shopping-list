bb739e879fff77636b0d17eef431e97610d949f4
Reese Avery Kling <raveryk@gmail.com>
2021-04-29 16:12:20 -0500

    Added loop through array to render function.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import Header from '../Header/Header.jsx'
 import './App.css';
 import ShoppingList from '../ShoppingList/ShoppingList'
--- src/components/ShoppingList/ShoppingList.jsx
+++ src/components/ShoppingList/ShoppingList.jsx
@@ -1,7 +1,10 @@
 import axios from 'axios';
+import {useState} from 'react';
 
 
 function ShoppingList() {
+
+    const [shoppingList, setShoppingList] = useState([])
 
     axios({
         method: 'GET',
@@ -9,17 +12,20 @@ function ShoppingList() {
     })
     .then(response => {
         console.log('You got items from the server:', response.data);
-
+        //Setting shoppingList to retain data coming back from server.
+        setShoppingList(response.data);
     })
     .catch(error => {
         console.log('Something went wrong with GET:', error);
-
     })
 
 
     return(
         <div>
-
+            <h1>Shopping List</h1>
+            {shoppingList.map(item => 
+                (<div key={item.id}>{item.name}{item.quantity}{item.unit} <DeleteItem id={item.id}/> </div>)
+                ) }
         </div>
 
     )
@@ -27,3 +33,4 @@ function ShoppingList() {
 
 
 export default ShoppingList;
+
