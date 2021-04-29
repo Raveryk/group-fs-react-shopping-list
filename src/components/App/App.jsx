4f5db101a84ec7eb6be5f627b1b9d52b6985bc51
Reese Avery Kling <raveryk@gmail.com>
2021-04-29 16:41:04 -0500

    Put GET into App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,16 +2,40 @@ import React from 'react';
 import Header from '../Header/Header.jsx'
 import './App.css';
 import ShoppingList from '../ShoppingList/ShoppingList'
+import axios from 'react';
+import {useState} from 'react';
 
 
 function App() {
+
+    const [shoppingList, setShoppingList] = useState([])
+    //function to get groceries from server
+    const getGroceries = () => {
+
+        axios({
+            method: 'GET',
+            url: '/list'
+        })
+        .then(response => {
+            console.log('You got items from the server:', response.data);
+            //Setting shoppingList to retain data coming back from server.
+            setShoppingList(response.data);
+        })
+        .catch(error => {
+            console.log('Something went wrong with GET:', error);
+        })
+    
+    }
+
+
+
     return (
         <div className="App">
             <Header />
             <main>
                 <p>Under Construction...</p>
 
-            <ShoppingList />
+            <ShoppingList shoppingList={shoppingList} getGroceries={getGroceries}/>
             </main>
         </div>
     );
--- src/components/ShoppingList/ShoppingList.jsx
+++ src/components/ShoppingList/ShoppingList.jsx
@@ -1,30 +1,13 @@
-import axios from 'axios';
-import {useState} from 'react';
 
 
-function ShoppingList() {
 
-    const [shoppingList, setShoppingList] = useState([])
-
-    axios({
-        method: 'GET',
-        url: '/list'
-    })
-    .then(response => {
-        console.log('You got items from the server:', response.data);
-        //Setting shoppingList to retain data coming back from server.
-        setShoppingList(response.data);
-    })
-    .catch(error => {
-        console.log('Something went wrong with GET:', error);
-    })
-
+function ShoppingList(props) {
 
     return(
         <div>
             <h1>Shopping List</h1>
-            {shoppingList.map(item => 
-                (<div key={item.id}>{item.name}{item.quantity}{item.unit} <DeleteItem id={item.id}/> </div>)
+            {props.shoppingList.map(item => 
+                (<div key={item.id}>{item.name}{item.quantity}{item.unit} <DeleteItem id={item.id}/> <BuyGroceries id={item.id} /></div>)
                 ) }
         </div>
 
